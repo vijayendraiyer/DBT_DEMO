3ca41918565ddaba20e3ae15c1a0d66f3e219d48
Vijayendra Iyer <vijayendraiyer@gmail.com>
2025-12-26 06:20:42 +0000

    Macro examples added

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,20 +4,8 @@ with
             to_timestamp(started_at) as started_at,
             date(to_timestamp(started_at)) as date_started_at,
             hour(to_timestamp(started_at)) as hour_started_at,
-            case
-                when dayname(to_timestamp(started_at)) in ('Sat', 'Sun')
-                then 'WEEKEND'
-                else 'BUSIENSSDAY'
-            end as daytype,
-            case
-                when month(to_timestamp(started_at)) in (12, 1, 2)
-                then 'WINTER'
-                when month(to_timestamp(started_at)) in (3, 4, 5)
-                then 'SPRING'
-                when month(to_timestamp(started_at)) in (6, 7, 8)
-                then 'SUMMER'
-                else 'AUTUMN'
-            end as station_of_year
+            {{ day_type("STARTED_AT") }} as daytype,
+            {{ get_season("STARTED_AT") }} as station_of_year
         from {{ source("demo", "bike") }}
         where started_at != 'started_at'
     )
